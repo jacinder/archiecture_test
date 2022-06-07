393f355bfd1b8dadcd9cea040f6fa8b251c174b8
Goeun Lee <goeunlee.st@gmail.com>
2022-06-07 10:30:09 +0900

    Update deploy.sh

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "> Deploy index.html"
-sudo cp -rf /home/ec2-user/dist/index.html /var/www/html/index.html
+sudo cp /home/ec2-user/dist/index.html /usr/share/nginx/html/index.html
